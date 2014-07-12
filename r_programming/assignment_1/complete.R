be820f806b99b08bd15cc28efbb7ebe4c98c73a8
samarthbhargav <samarth.bhargav92@gmail.com>
2014-07-12 23:26:19 +0530

    Added complete and corr

--- r_programming/assignment_1/complete.R
+++ r_programming/assignment_1/complete.R
@@ -1,15 +1,13 @@
 complete <- function(directory, id = 1:332) {
-  ## 'directory' is a character vector of length 1 indicating
-  ## the location of the CSV files
-  
-  ## 'id' is an integer vector indicating the monitor ID numbers
-  ## to be used
-  
-  ## Return a data frame of the form:
-  ## id nobs
-  ## 1  117
-  ## 2  1041
-  ## ...
-  ## where 'id' is the monitor ID number and 'nobs' is the
-  ## number of complete cases
+  ids <- vector()
+  nobs <- vector()
+  for(i in id) {
+    ptrn <- paste(directory, "/", sprintf("%03d", i), ".csv", sep="")
+    data <- read.csv(ptrn)
+    count <- nrow(data[!is.na(data$sulfate) & !is.na(data$nitrate), ])
+    ids <- c(ids, i)
+    nobs <- c(nobs, count)
+  }
+  to_return <- data.frame(ids,nobs)
+  return(to_return)
 }
--- r_programming/assignment_1/corr.R
+++ r_programming/assignment_1/corr.R
@@ -1,3 +1,4 @@
+
 corr <- function(directory, threshold = 0) {
   ## 'directory' is a character vector of length 1 indicating
   ## the location of the CSV files
@@ -8,4 +9,16 @@ corr <- function(directory, threshold = 0) {
   ## nitrate and sulfate; the default is 0
   
   ## Return a numeric vector of correlations
+  id <- 1:332
+  compl = complete(directory, id)
+  compl <- compl[compl$nobs > threshold,]
+  cr = vector()
+  for(i in compl$id) {
+    ptrn <- paste(directory, "/", sprintf("%03d", i), ".csv", sep="")
+    data <- read.csv(ptrn)
+    cleaned <- data[!is.na(data$sulfate) & !is.na(data$nitrate), ]
+    correlation <- cor(cleaned$sulfate, cleaned$nitrate)
+    cr <- c(cr, correlation)
+  }
+  return(cr)
 }
